a5582275918bde1b5685ec83b086060b9891f705
Ramon <pcnay2003@gmail.com>
2019-11-09 00:28:16 -0700

    Se disena el Login utilizando HMTL y CSS solamente

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php
-  
+<!DOCTYPE html>
+  <html lang="es">
+  <head>
+    <meta charset = "UTF-8">
+    <title>Login Sistema De Facturacion</title>
+    <link rel="stylesheet" type ="text/css" href="css/style.css">
+  </head>
+  <body>
+    <section id = "container">
+      <form action = "" method ="post">
+        <h3>Iniciar Sesion</h3>
+        <img src="img/login.png" alt = "Login">
+        <input type="text" name= "usuario" placeholder="Usuario">
+        <input type="password" name= "clave" placeholder="Contraseña">
+        <p class="alert"></p>
+        <input type="submit" value ="INGRESAR">
 
-  
-?>
+      </form>
+    </section>
+
+  </body>
+
+</html>
